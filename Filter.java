4c65f9b14984fc60a56a5828a02b5bda695f8311
Sevara Razzakova <razzakov@usc.edu>
2018-11-14 00:28:09 -0800

    updated servlet for the filter

--- Filter.java
+++ Filter.java
@@ -20,11 +20,14 @@ public class Filter extends HttpServlet {
 		
 		System.out.println("In the sr_servlet------------------->");
        //get filters
-		String age = request.getParameter("age");
+		String age1 = request.getParameter("age1");
+		String age2 = request.getParameter("age2");
 		String gender = request.getParameter("gender");
 		String major = request.getParameter("major");
+		String idealDate = request.getParameter("idealDate");
+	    String yr = request.getParameter("yr");
 		System.out.println("major= " + major);
-		System.out.println("age= " + age);
+		System.out.println("age1= " + age1);
 		System.out.println("gender= " + gender);
 		
 		PrintWriter out = response.getWriter();
@@ -36,39 +39,106 @@ public class Filter extends HttpServlet {
 	    ResultSet friends = null;
 
 		PreparedStatement preparedStatement = null;
+		String query = "";
 		String insertTableSQL = "";
+		boolean flag = false;
 							
 	    try {
 	    	Class.forName("com.mysql.jdbc.Driver");
 	        conn = DriverManager.getConnection("jdbc:mysql://localhost/trojanmingle?user=root&password=root&useSSL=false&AllowPublicKeyRetrieval=True");
 	        
 	        //search users in database based on filters
-	        insertTableSQL = "SELECT * from users where major = '" + major + "' and age = '" + age  + "' and gender= '" + gender + "'";
-	        
-			preparedStatement = conn.prepareStatement(insertTableSQL);  
-			rs = preparedStatement.executeQuery();
-			//array of users that meet criteria
-	        ArrayList<String> results = new ArrayList<String>();
-       
-	        while (rs.next()) {
-	        	String potential = rs.getString("fname");
-	        	results.add(potential);
+	        insertTableSQL = "SELECT * from users where major = '" + major + "' and age = '" + age1  + "' and gender= '" + gender + "'";
+	        System.out.println(insertTableSQL);
+	        query = "SELECT * from users where ";
+	        //ArrayList<String> conditions = new ArrayList<String>();
+	        if(!gender.equals("Choose...")) {
+	        	query += "gender = '" + gender + "'";
+	        	flag = true;
 	        }
+	        if(!age1.equals("Age...") &&  !age2.equals("Age...")) {
+	        	if(flag) {
+	        	 query += " and age between '" + age1 + "'and'" + age2 + "'";
+	        	} else {
+	        		query += " age between '" + age1 + "'and'" + age2 + "'";
+	        		flag = true;
+	        	}
+	        } else if(!age1.equals("Age...") &&  age2.equals("Age...")) {
+	        	if(flag) {
+	        	 query += " and age  = '" + age1 + "'";
+	        	} else {
+	        		 query += " age  = '" + age1 + "'";
+	        		 flag = true;
+	        	}
+	        } else if(age1.equals("Age...") &&  !age2.equals("Age...")) {
+	        	if(flag) {
+		        	 query += " and age  = '" + age2 + "'";
+		        	} else {
+		        		 query += " age  = '" + age2 + "'";
+		        		 flag = true;
+		        	}
+	        }
+	        if(!major.equals("Choose...")) {
+	        	if(flag) {
+		        	 query += " and major  = '" + major + "'";
+		        } else {
+	        		 query += " major  = '" + major + "'";
+	        		 flag = true;
+		        }
+	        }
+	        if(!yr.equals("Choose...")) {
+	        	if(flag) {
+		        	 query += " and yr  = '" + yr + "'";
+		        } else {
+	        		 query += " yr  = '" + yr + "'";
+	        		 flag = true;
+		        }
+	        }
+	        if(!idealDate.equals("Choose...")) {
+	        	if(flag) {
+		        	 query += " and idealDate  = '" + idealDate + "'";
+		        } else {
+	        		 query += " idealDate  = '" + idealDate + "'";
+	        		 flag = true;
+		        }
+	        }
+	        System.out.println(query);
+	       
+
+	        preparedStatement = conn.prepareStatement(query);  
+			rs = preparedStatement.executeQuery();
+	        if(!rs.next()) {
+	        	//no users match the criteria
+	        	String result = "No results matching your criteria";
+	        	out.println("<figcaption id = \"cap\">" + result  + "</figcaption>");	
 	        
-			//print the name of user
-			for (int i = 0; i < results.size(); i++) {
-				
-		        insertTableSQL = "SELECT * from users where fname = '" + results.get(i) + "'";
-				preparedStatement = conn.prepareStatement(insertTableSQL);  
-				friends = preparedStatement.executeQuery();
-				friends.next();
-				String name = friends.getString("fname");
-				out.println("<figcaption id = \"cap\">" + name  + "</figcaption>");								
-			}					
-			System.out.println("passed results...");
+	        	
+	        } else {
+	        
+                rs.beforeFirst();
+		        ArrayList<String> results = new ArrayList<String>();
+		        System.out.println("Here");
+		        while (rs.next()) {
+		        	String potential = rs.getString("fname");
+		        	System.out.println(potential);
+		        	results.add(potential);
+		        }
+		        for (int i = 0; i < results.size(); i++) {
+					
+			        query = "SELECT * from users where fname = '" + results.get(i) + "'";
+					preparedStatement = conn.prepareStatement(query);  
+					friends = preparedStatement.executeQuery();
+					friends.next();
+					String name = friends.getString("fname");
+					out.println("<figcaption id = \"cap\">" + name  + "</figcaption>");								
+				}					
+				System.out.println("passed results...");
+	        }
+			
 	        
 	    } catch (SQLException sqle) {
-	        System.out.println("Here1");	    	
+	        System.out.println("Here1");
+	        
 	    	System.out.println (sqle.getMessage());
 	    } catch (ClassNotFoundException cnfe) {
 	        System.out.println("Here2");	    	
